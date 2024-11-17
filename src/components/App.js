5aa583631d1c1bbe161d91cf4389a69982043669
MeghanaSubramanyam <megha@gmail.com>
2024-11-17 15:32:56 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import Counter from './Counter';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
